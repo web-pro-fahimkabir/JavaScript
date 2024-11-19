9578edce86761496586bd93c41a91195249a8a74
web-pro-fahimkabir <fahimkabir1122@yahoo.com>
2024-11-19 18:58:16 +0600

    h

--- statement.js
+++ statement.js
@@ -1,4 +1,12 @@
-var a=10;
+/*var a=10;
 var b=12;
-var c = a+b;
-console.log(c);
+var c = a +  b;
+console.log(c);    */
+
+let a=9,b=10;
+if (a < b) {
+    console.log("a is less than b"); 
+}
+else{
+    console.log("b is less than a"); 
+}
